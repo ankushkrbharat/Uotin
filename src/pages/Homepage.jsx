f8e389d655fc9222f073eb597df61839b90eca71
Ankush <ankushvermabharat@gmail.com>
2023-09-09 14:58:20 +0530

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -26,24 +26,41 @@ const Homepage = () => {
             Cupiditate, magni?
           </p>
           <div className="flex gap-2 py-2 rounded-full items-center text-l mt-6">
-          <Link to={'/create'} className="flex gap-2 bg-primary px-5 py-2 rounded-full items-center text-3xl hover:bg-secondary" >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-  <path fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z" clip-rule="evenodd" />
-  <path fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z" clip-rule="evenodd" />
-</svg>
+            <Link
+              to={"/create"}
+              className="flex gap-2 bg-primary px-5 py-2 rounded-full items-center text-3xl hover:bg-secondary"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"
+                  clip-rule="evenodd"
+                />
+              </svg>
 
-
-
-            <div>Create</div>
-          </Link>
-          <p className="hover:text-opacity-75 text-dark cursor-pointer hover:underline">Read more About us</p>
-        </div></div>
+              <div>Create</div>
+            </Link>
+            <p className="hover:text-opacity-75 text-dark cursor-pointer hover:underline">
+              Read more About us
+            </p>
+          </div>
+        </div>
         <div className="w-full">
           <img src={chart} alt="chart" />
         </div>
-      </div> <Footer/>
+      </div>{" "}
+      <Footer />
     </div>
-   
   );
 };
 
