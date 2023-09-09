29e0d3bffb1f4e57bf9c6186e4d501656e6b6974
Ankush <ankushvermabharat@gmail.com>
2023-09-09 11:09:30 +0530

    right image

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import chart from './account1.png'
 
 const Homepage = () => {
   return (
     <div><Header/>
     <div className='w-full h-screen flex'>
       <div className="w-full">zz</div>
-      <div className="w-full">a</div>
+      <div className="w-full"><img src={chart} alt="chart"  /></div>
     </div>
     </div>
   )
