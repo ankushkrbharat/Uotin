21fd78481b7a626cc9a775a5f80505b623c5aabc
Ankush <ankushvermabharat@gmail.com>
2023-09-09 11:47:20 +0530

    created create page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/Homepage'
+import Create from './pages/Create'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path={'/'} element={<Homepage/>} />
+      <Route path={'/create'} element={<Create/>} />
     </Routes>
   )
 }
---
+++ src/pages/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const Create = () => {
+  return (
+    <div><Header/>
+    <div>create page</div>
+    </div>
+  )
+}
+
+export default Create
