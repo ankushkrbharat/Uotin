266c28d4d34ec0952e43194ea2e4333a3c5fdb15
Ankush <ankushvermabharat@gmail.com>
2023-09-10 17:09:36 +0530

    grid

--- src/Components/R1.jsx
+++ src/Components/R1.jsx
@@ -10,13 +10,31 @@ const R1 = () => {
   const widthc=window.innerWidth;
   // const heightc=window.innerHeight;
   const heightc=widthc*(683.35/1151.2)
+ const size=Math.floor(widthc/50);
+ const t=Math.floor(heightc/size)
+ 
+  const div2=[]
+
+  for (let i = 50; i < 50+t; i++) {
+    div2.push(<div key={i} style={{width:size,height:size}} >s</div>);
+  }
+
+  const n=50  // Change this to the number of divs you want to insert
+  const divs = [];
+
+  for (let i = 0; i < n; i++) {
+    divs.push(<div key={i} style={{width:size,height:size}} >{div2}</div>);
+  }
+
+  
+
   return (
     <div>
       <Header />
       <div>
         <div className="flex justify-center"><input type="color" onChange={(e)=>{setColor(e.target.value)}}  value={color}/></div>
         <div className="relative"><div className="canvas1"><img src={RM} alt="img" className="w-screen" /></div>
-        <div className="absolute top-0 left-0" style={{width:widthc,height:heightc}}></div></div>
+        <div className="absolute top-0 left-0 flex" style={{width:widthc,height:heightc}}>{divs}</div></div>
 
       </div>
       <Footer />
